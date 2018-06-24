0daf39cd49680e4fb717e5f8d28fcd6a9c5e5827
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2018-06-24 23:48:16 +0300

    Add the old version of nanoInkscape to different folder

---
+++ nanoInkscape2/bezier.js
@@ -0,0 +1,145 @@
+"use strict";
+nanoInk.addTool("bezier", {
+	oldControlPoint: undefined,
+	tempCurve: "",
+	closepath: true,
+
+	mainInit: (function() {
+		
+	}),
+	init: (function() {
+		nanoInk.active = undefined;
+		this.curveHelper = nanoInk.newElem("line", {"stroke": "#000", "style": "visibility: hidden"});
+		this.curveHelper2 = nanoInk.newElem("circle", {"stroke": "#000", "fill": "transparent", "r": 4, "style": "visibility: hidden"});
+	}),
+	uninit: (function() {
+		if(this.tempCurve != "") {
+
+			nanoInk.newAttr(nanoInk.active, {
+				"class": "",
+				"fill": nanoInk.fill,
+				"stroke": nanoInk.stroke,
+				"d": this.tempCurve
+			});
+
+			nanoInk.remElem(this.zHelper);
+			this.tempCurve = "";
+			this.oldControlPoint = undefined;
+
+		}
+		nanoInk.remElem(this.curveHelper);
+		nanoInk.remElem(this.curveHelper2);
+		this.tempCurve = "";
+	}),
+	mouseMove: (function() {
+		if(this.tempCurve != "") {
+			var data = this.tempCurve;
+			if(this.oldControlPoint != undefined) {
+				data += " C";
+				data += this.oldControlPoint + " ";
+				data += nanoInk.pointerX +","+ nanoInk.pointerY + " ";
+				data += nanoInk.pointerX +","+ nanoInk.pointerY;
+			} else {
+				data += " L";
+				data += nanoInk.pointerX +","+ nanoInk.pointerY;
+			}
+			nanoInk.active.setAttributeNS(null, "d", data);
+		}
+	}),
+	mouseDrag: (function(initialize) {
+		nanoInk.newAttr(this.curveHelper, {
+			"style": "visibility: visible",
+			"x1": nanoInk.pointerStartX,
+			"y1": nanoInk.pointerStartY,
+			"x2": nanoInk.pointerEndX,
+			"y2": nanoInk.pointerEndY
+		});
+		nanoInk.newAttr(this.curveHelper2, {
+			"style": "visibility: visible",
+			"cx": nanoInk.pointerEndX,
+			"cy": nanoInk.pointerEndY
+		});
+		var data = this.tempCurve;
+		if(this.oldControlPoint != undefined) {
+			data += " C";
+			data += this.oldControlPoint + " ";
+			data += nanoInk.pointerStartX - (nanoInk.pointerEndX - nanoInk.pointerStartX) + ",";
+			data += nanoInk.pointerStartY - (nanoInk.pointerEndY - nanoInk.pointerStartY) + " ";
+			data += nanoInk.pointerStartX +","+ nanoInk.pointerStartY;
+		} else if(this.tempCurve == "") {
+			data += "M";
+			data += nanoInk.pointerStartX +","+ nanoInk.pointerStartY;
+			data += " L";
+			data += nanoInk.pointerX +","+ nanoInk.pointerY;
+
+		} else {
+			data += " L";
+			data += nanoInk.pointerX +","+ nanoInk.pointerY;
+		}
+		nanoInk.active.setAttributeNS(null, "d", data);
+	}),
+	mouseDown: (function() {
+		nanoInk.newAttr(this.curveHelper, {"style": "visibility: hidden"});
+		nanoInk.newAttr(this.curveHelper2, {"style": "visibility: hidden"});
+
+		if(nanoInk.eTarget === this.zHelper) {
+			this.closepath = true;
+		} else if(this.closepath == true) {
+			nanoInk.active = nanoInk.newElem("path", {"class": "bezier"});
+			this.zHelper = nanoInk.newElem("rect", {
+				"x": nanoInk.pointerStartX-3.5,
+				"y": nanoInk.pointerStartY-3.5,
+				"height": 6, "width": 6,
+				"id": "helperClosepath"
+			});
+			this.closepath = false;
+		}
+	}),
+	mouseUp: (function() {
+		if(nanoInk.active == undefined) return;
+
+		//data = nanoInk.active.getAttributeNS(null, "d");
+		if(this.closepath) {
+			nanoInk.pointerStartX = parseFloat(this.zHelper.getAttributeNS(null, "x"))+3.5;
+			nanoInk.pointerStartY = parseFloat(this.zHelper.getAttributeNS(null, "y"))+3.5;
+		}
+		if(this.oldControlPoint != undefined) {
+			this.tempCurve += " C";
+			this.tempCurve += this.oldControlPoint + " ";
+			this.tempCurve += nanoInk.pointerStartX - (nanoInk.pointerEndX - nanoInk.pointerStartX) + ",";
+			this.tempCurve += nanoInk.pointerStartY - (nanoInk.pointerEndY - nanoInk.pointerStartY) + " ";
+			this.tempCurve += nanoInk.pointerStartX +","+ nanoInk.pointerStartY;
+		}
+		if(this.closepath) {
+
+			nanoInk.newAttr(this.curveHelper, {"style": "visibility: hidden"});
+			nanoInk.newAttr(this.curveHelper2, {"style": "visibility: hidden"});
+			nanoInk.newAttr(nanoInk.active, {
+				"class": "",
+				"fill": nanoInk.fill,
+				"stroke": nanoInk.stroke,
+				"d": this.tempCurve +" z"
+			});
+
+			nanoInk.remElem(this.zHelper);
+			this.tempCurve = "";
+			this.oldControlPoint = undefined;
+			return;
+
+		} else if(this.tempCurve == "") {
+			this.tempCurve += "M";
+			this.tempCurve += nanoInk.pointerX +","+ nanoInk.pointerY;
+
+		} else if(this.oldControlPoint == undefined) {
+			this.tempCurve += " L";
+			this.tempCurve += nanoInk.pointerEndX +","+ nanoInk.pointerEndY;
+		} else {
+			this.oldControlPoint = undefined;
+		}
+		nanoInk.active.setAttributeNS(null, "d", this.tempCurve);
+
+		if(nanoInk.pointerDrag) {
+			this.oldControlPoint = nanoInk.pointerEndX +","+ nanoInk.pointerEndY;
+		}
+	})
+});
---
+++ nanoInkscape2/core.js
@@ -0,0 +1,161 @@
+const SVG_NS = "http://www.w3.org/2000/svg";
+
+function print_obj(obj) {
+	var str = "";
+	for(i in obj) {
+		str += i +"=>"+ obj[i] +", ";
+	}
+	alert(str);
+}
+
+var nanoInk = {
+//general
+	tool: "select",
+	toolList: {},
+	active: undefined,
+
+//events
+	pointerX: 0,
+	pointerY: 0,
+	pointerDragX: 0,
+	pointerDragY: 0,
+	pointerDown: false,
+	pointerDrag: false,
+
+//styles
+	fill: "transparent",
+	stroke: "#000",
+	
+//methods
+	init: (function() {
+		this.toolbar = document.getElementById("toolbar");
+		this.canvas = document.getElementById("canvas");
+		this.draw = document.getElementById("drawDiv");
+		this.statusbar = document.getElementById("statusbar");
+		document.querySelector("#fill_color").addEventListener("change", function() {
+			nanoInk.fill = this.value;
+		});
+		document.querySelector("#stroke_color").addEventListener("change", function() {
+			nanoInk.stroke = this.value;
+		});
+	}),
+	newElem: (function(tag, attr) {
+		var elem = document.createElementNS(SVG_NS, tag);
+		for(i in attr) {
+			elem.setAttributeNS(null, i, attr[i]);
+		}
+		return this.canvas.appendChild(elem);
+	}),
+	remElem: (function(elem) {
+		this.canvas.removeChild(elem);
+	}),
+	newAttr: (function(elem, attr) {
+		for(i in attr) {
+			elem.setAttributeNS(null, i, attr[i]);
+		}
+	}),
+	mouseDown: (function(e) {
+		if(this.toolList.length == 0) return;
+		this.pointerStartX = e.clientX - this.draw.offsetLeft;
+		this.pointerStartY = e.clientY - this.draw.offsetTop;
+		this.eTarget = e.target;
+		this.pointerDown = true;
+		this.pointerDrag = false;
+
+		this.toolList[this.tool].mouseDown();
+	}),
+	mouseMove: (function(e) {
+		if(this.toolList.length == 0) return;
+		if(this.pointerDown == true) {
+			this.pointerDrag = true;
+
+			this.pointerEndX = e.clientX - this.draw.offsetLeft;
+			this.pointerEndY = e.clientY - this.draw.offsetTop;
+
+			this.toolList[this.tool].mouseDrag();
+		} else {
+			this.pointerX = e.clientX - this.draw.offsetLeft;
+			this.pointerY = e.clientY - this.draw.offsetTop;
+
+			this.toolList[this.tool].mouseMove();
+		}
+	}),
+	mouseUp: (function(e) {
+		if(this.toolList.length == 0) return;
+		this.eTarget = e.target;
+		this.pointerDown = false;
+		if(this.pointerDrag = true) {
+			this.pointerEndX = e.clientX - this.draw.offsetLeft;
+			this.pointerEndY = e.clientY - this.draw.offsetTop;
+
+		} else { this.pointerEndX = this.pointerEndY = undefined; }
+
+		this.toolList[this.tool].mouseUp();
+	}),
+	addTool: (function(toolName, toolObj) {
+		this.toolList[toolName] = toolObj;
+		this.toolList[toolName].mainInit();
+	}),
+	changeTool: (function(toolName) {
+		if(this.toolList.length == 0) return;
+		this.toolList[this.tool].uninit();
+		for(var i=0; i < this.toolbar.getElementsByTagName("button").length; i++) {
+			this.toolbar.getElementsByTagName("button")[i].className = " tools";
+		}
+		document.getElementById("tool_"+toolName).className += " toolActive";
+		
+		this.tool = toolName;
+		this.toolList[this.tool].init();
+	}),
+};
+
+nanoInk.addTool("select", {
+	move: false,
+	boxSelection: false,
+	oldX: 0,
+	oldY: 0,
+
+	mainInit: (function() {
+
+	}),
+	init: (function() {
+	}),
+	uninit: (function() {
+
+	}),
+	mouseMove: (function() {
+		
+	}),
+	mouseDrag: (function() {
+		if(this.move == true) {
+			nanoInk.statusbar.textContent = (nanoInk.pointerEndX - nanoInk.pointerStartX) +", "+ (nanoInk.pointerEndY - nanoInk.pointerStartY);
+
+			nanoInk.newAttr(nanoInk.active, {
+				"transform": "translate("+ (this.oldX + nanoInk.pointerEndX - nanoInk.pointerStartX) +
+				             ", "+ (this.oldY + nanoInk.pointerEndY - nanoInk.pointerStartY) +")"
+			});
+		}
+	}),
+	mouseDown: (function() {
+		if(nanoInk.eTarget.tagName != "svg") {//move
+			this.move = true;
+			this.boxSelection = false;
+			nanoInk.active = nanoInk.eTarget;
+			if(nanoInk.active.hasAttributeNS(null, "transform")) {
+				var oldValues = /translate\(([^,]*), ([^)]*)\)/.exec(nanoInk.active.getAttributeNS(null, "transform"));
+				//alert(nanoInk.active.getAttributeNS(null, "transform"));
+				this.oldX = parseFloat(oldValues[1]);
+				this.oldY = parseFloat(oldValues[2]);
+			} else {
+				this.oldX = this.oldY = 0;
+			}	
+		} else {//select
+			this.move = false;
+			this.boxSelection = true;
+		}
+	}),
+	mouseUp: (function() {
+		this.move = this.boxSelection = false;
+		
+	})
+});
