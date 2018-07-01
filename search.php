7029e07993ae899141b4040f12e76c7f2a0e9f64
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2018-07-02 01:29:23 +0300

    Add little better searching code

--- search.php
+++ search.php
@@ -22,8 +22,28 @@ class Search implements View {
 		return $results;
 	}
 
-	private function getHeaders($content) {
-		preg_match_all("/<h[1-9]>(.*)<\/h[1-9]>/", $content, $headers, PREG_SET_ORDER);
+	private function normalize($token) {
+		if (class_exists("Normalizer")) {
+			return Normalizer::normalize(mb_strtolower($token, "UTF-8"));
+		} else {
+			return mb_strtolower($token, "UTF-8");
+		}
+	}
+
+	private function getTokenizer($content) {
+		preg_match_all("/<\/?[a-z1-9]+(?:\s[^>]*)?>|[\\p{L}]+/u", $content, $tokens, PREG_SET_ORDER);
+		$textTokens = [];
+		foreach ($tokens as $match) {
+			$token = $match[0];
+			if ($token[0] != "<" && strlen(trim($token)) != 0) {
+				$textTokens[] = $this->normalize($token);
+			}
+		}
+		return $textTokens;
+	}
+
+	static function getHeaders($content) {
+		preg_match_all("/<h[1-9][^>]*>(.*)<\/h[1-9]>/", $content, $headers, PREG_SET_ORDER);
 		return $headers;
 	}
 
@@ -34,12 +54,21 @@ class Search implements View {
 		$generated = array();
 		foreach ($results as $page) {
 			$content = $page->generateContent();
-			$headers = $this->getHeaders($content);
+			$textTokens = $this->getTokenizer($content);
+			//var_dump($textTokens);
+			$headers = $this::getHeaders($content);
 
+			$position = array_search($query, $textTokens);
+			$offset = max(0, $position - 4);
+			$fragment = array_slice($textTokens, max(0, $position - 4), $position - $offset);
+			$fragment[] = "<strong>" . $textTokens[$position] . "</strong>";
+			$offset = $position + 1;
+			$fragment = array_merge($fragment, array_slice($textTokens, $offset,
+				min(count($textTokens), $position + 4) - $offset));
 			$generated[] = array(
 				"url" => "/" . $page->getPageId(),
 				"title" => $headers[0][1],
-				"excerpt" => "test"
+				"excerpt" => join(" ", $fragment)
 			);
 		}
 
