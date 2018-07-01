8ea64f26ad0370bc977e8a27f54827c5c7762f47
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2018-07-01 15:47:18 +0300

    Implement the search page as Page object

--- search.php
+++ search.php
@@ -2,30 +2,37 @@
 
 require_once("page.php");
 
-if (!isset($_GET["q"])) {
-	die("Expect query 'q' GET parameter");
-}
+class Search implements View {
+	function getContent($lang) {
+		global $pageMap;
+		if (!isset($_GET["q"])) {
+			die("Expect query 'q' GET parameter");
+		}
 
-$query = $_GET["q"];
-$isEmpty = true;
-echo "<div class=\"section\">";
-echo "<h1>Haku tulokset</h1>";
+		$query = $_GET["q"];
+		$isEmpty = true;
+		echo "<div class=\"section\">";
+		echo "<h1>Haku tulokset</h1>";
 
-$results = array();
-foreach ($pageMap as $page) {
-	if ($query && strpos($page->getContent(), $query)) {
-		$results[] = $page;
+		$results = array();
+		foreach ($pageMap as $page) {
+			if ($query && strpos($page->getContent(), $query)) {
+				$results[] = $page;
+			}
+		}
+		if (empty($results)) {
+			echo "<p>No results found</p>";
+		} else {
+			echo "<ol>";
+			foreach ($results as $page) {
+				echo "<li><a href=\"/" . $page->getPageId() . "\">" . $page->getPageId() . "</a></li>";
+			}
+			echo "</ol>";
+		}
+		echo "</div>";
 	}
 }
-if (empty($results)) {
-	echo "<p>No results found</p>";
-} else {
-	echo "<ol>";
-	foreach ($results as $page) {
-		echo "<li><a href=\"/" . $page->getPageId() . "\">" . $page->getPageId() . "</a></li>";
-	}
-	echo "</ol>";
-}
-echo "</div>";
+$page = new Search();
+echo $page->getContent("fi");
 
 ?>
